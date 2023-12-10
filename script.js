95e9cefc3c0d0c499380a0f0be6c09839c27a3d5
Lakshmi priyanka <89516421+lakshmipriyankaimadabattina@users.noreply.github.com>
2023-12-10 12:29:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Create a user schema
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/');
+        } else {
+            res.send('Registration successful!');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
